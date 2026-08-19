7806f40a0b7dd0b5cad09e7fbcf3f35c7f91ae0a
rubymadhur-code <rudhrakshi12@gmail.com>
2026-08-19 22:13:17 +0530

    longest common prefix

--- #14-longest-common-prefix/longest-common-prefix.js
+++ #14-longest-common-prefix/longest-common-prefix.js
@@ -2,8 +2,6 @@
 
 // If there is no common prefix, return an empty string "".
 
- 
-
 // Example 1:
 
 // Input: strs = ["flower","flow","flight"]
@@ -15,18 +13,17 @@
 // Explanation: There is no common prefix among the input strings.
 
 
+var longestCommonPrefix = function (strs) {
+  let prefix = "";
 
-var longestCommonPrefix = function(strs) {
-    let prefix = '';
-
-    for(let i = 0; i < strs[0].length; i++){
-        let char = strs[0][i];
-        for(let j = 1; j < strs.length; j++){
-            if(strs[j][i] !== char){
-                return prefix ;
-            }
-        }
-        prefix += char;
+  for (let i = 0; i < strs[0].length; i++) {
+    let char = strs[0][i];
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[j][i] !== char) {
+        return prefix;
+      }
     }
-    return prefix;
+    prefix += char;
+  }
+  return prefix;
 };
