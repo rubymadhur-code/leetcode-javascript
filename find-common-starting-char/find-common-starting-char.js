c2e872a1b0e6a30a25097bd3b2225cfe417f068c
rubymadhur-code <rudhrakshi12@gmail.com>
2026-08-19 22:23:24 +0530

    find common starting character

---
+++ find-common-starting-char/find-common-starting-char.js
@@ -0,0 +1,21 @@
+// Find the common starting characters
+
+let words = ["coding", "code", "coder"];
+
+function findStartChar(words) {
+  let answer = "";
+
+  for (let i = 0; i < words[0].length; i++) {
+    let char = words[0][i];
+
+    for (let j = 1; j < words.length; j++) {
+      if (words[j][i] !== char) {
+        return answer;
+      }
+    }
+    answer += char;
+  }
+  return answer;
+}
+
+console.log(findStartChar(words));
