969896e7fa0c4ddd5e807779f0f7c574efb9b0d1
LearnCodeEasy-b4p <rudhrakshi12@gmail.com>
2026-08-19 20:58:57 +0530

    longest-common-prefix

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,32 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+
+
+var longestCommonPrefix = function(strs) {
+    let prefix = '';
+
+    for(let i = 0; i < strs[0].length; i++){
+        let char = strs[0][i];
+        for(let j = 1; j < strs.length; j++){
+            if(strs[j][i] !== char){
+                return prefix ;
+            }
+        }
+        prefix += char;
+    }
+    return prefix;
+};
